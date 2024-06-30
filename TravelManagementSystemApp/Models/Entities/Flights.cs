9a65abcd8faedcd96946f737e48c28525b6dae24
rojag99 <GRoja@1999>
2024-06-30 16:16:17 +0530

    created flights and flightsschedules entities

--- TravelManagementSystemApp/Models/Entities/Flights.cs
+++ TravelManagementSystemApp/Models/Entities/Flights.cs
@@ -2,5 +2,15 @@
 {
     public class Flights
     {
+        [Key]
+        public int FlightID { get; set; }
+        public string FlightNumber { get; set; }
+        public string Airline { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public int TotalSeats { get; set; }
+        public int AvailableSeats { get; set; }
+        // Navigation property to represent the relationship with FlightsSchedules
+        public ICollection<Flightsschedules> Flightsschedules { get; set; }
     }
 }
